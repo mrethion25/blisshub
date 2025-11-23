8fa291532c87ec27d7947ee14f494e2ea6747a31
mrethion25 <worldof2512@gmail.com>
2025-11-23 14:11:58 +0530

    Update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-export default function GlobalError({ error }) {
+export default function GlobalError({
+  error,
+}: {
+  error: Error;
+}) {
   return (
     <html>
       <body style={{ padding: 20 }}>
         <h1 style={{ color: "red" }}>Server Component Error</h1>
-        <pre>{error?.message}</pre>
-        <pre>{error?.stack}</pre>
+        <pre>{error.message}</pre>
+        <pre>{error.stack}</pre>
       </body>
     </html>
   );
